c09995fcd4f84d013f9f338ef72082fc0c36bd3d
Krystian Stopka <kr6stopka@gmail.com>
2021-06-08 22:57:36 +0200

    background for neon

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const header = document.querySelector(`header`);
 const logo = document.querySelector(".logo");
 const navigation = document.querySelector("nav");
+const slantOne = document.querySelector(".slantOne");
+const slantTwo = document.querySelector(".slantTwo");
 
 window.addEventListener("scroll", () => {
     header.classList.toggle("wrap", window.scrollY > 0);
@@ -9,6 +11,11 @@ window.addEventListener("scroll", () => {
     } else {
         logo.innerHTML = "Krystian Stopka";
     }
+
+    const firstValue = -15 + window.scrollY / 45;
+    const secondValue = 15 + window.scrollY / -45;
+    slantOne.style.transform = `skewY(${firstValue}deg)`;
+    slantTwo.style.transform = `skewY(${secondValue}deg)`;
 });
 
 document.querySelector(".toggle").onclick = function () {
